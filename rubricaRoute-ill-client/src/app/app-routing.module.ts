22f2696517a09f2a973690b38dab11701d7c70dc
Emanuele1927 <35915808+Emanuele1927@users.noreply.github.com>
2021-02-23 14:38:31 +0100

    Update Main Page

--- rubricaRoute-ill-client/src/app/app-routing.module.ts
+++ rubricaRoute-ill-client/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CountPageComponent } from './count-page/count-page.component';
+import { ErrorMessagePageComponent } from './error-message-page/error-message-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { SearchPageComponent } from './search-page/search-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'login-page', pathMatch: 'full'},
+  {path: 'error-message-page', component: ErrorMessagePageComponent},
+  {path: 'main-page', component: MainPageComponent},
+  {path: 'search-page', component: SearchPageComponent},
+  {path: 'login-page', component: LoginPageComponent},
+  {path: 'count-page', component: CountPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
