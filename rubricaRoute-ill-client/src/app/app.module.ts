bb89829c3f3d8118d9fd3684a17569368721ba80
Giulia2Gargano <giulia2gargano@gmail.com>
2021-02-23 13:39:29 +0100

    Client Created

---
+++ rubricaRoute-ill-client/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ErrorMessagePageComponent } from './error-message-page/error-message-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { SearchPageComponent } from './search-page/search-page.component';
+import { CountPageComponent } from './count-page/count-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginPageComponent,
+    ErrorMessagePageComponent,
+    MainPageComponent,
+    SearchPageComponent,
+    CountPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
